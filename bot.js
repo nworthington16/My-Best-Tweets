0bb8af8429955e6fdb997acad2479a306eb25b12
Nick Worthington <nworthington16@gmail.com>
2018-06-18 22:45:07 -0400

    function to grab tweets with > 10 faves

--- bot.js
+++ bot.js
@@ -5,48 +5,25 @@ var T = new Twit(credentials);
 // console.log("The bot is working \n");
 
 var params = {screen_name: '16nworthington', count: 200};
-// var lowest_id = 0;
-// for (var i = 0; i < 4; i++) {
-//   T.get('statuses/user_timeline', params, function(err, data, response) {
-//     var tweet = data[186];
-//     if (tweet !== undefined) {
-//       console.log(tweet.text);
-//       lowest_id = tweet.id;
-//     }
-//   });
-//   console.log(i);
-//   params.max_id = lowest_id;
-// }
-let promise = T.get('statuses/user_timeline', params, function(err, data, response) {
-  console.log(data[0].text);
-});
-console.log(promise);
-// params.max_id = 685875153008508900;
-// T.get('statuses/user_timeline', params, function(err, data, response) {
-//   console.log(data[0].text);
-// });
 
-// params.max_id = 685875153008508900;
-// T.get('statuses/user_timeline', params, callback);
-// console.log("Lowest ID: ", lowest_id);
+function doStuff(i, params) {
+  if (i < 3200) {
+    T.get('statuses/user_timeline', params, function(err, data, response) {
 
-function callback(err, data, response) {
-  if (i < 16) {
-    console.log("Inside if statement");
-    for (var i = 0; i < params.count; i++) {
-      var tweet = data[i];
-      if (tweet !== undefined) {
-        if (!tweet.hasOwnProperty("retweeted_status")) {
-          if (tweet.favorite_count  > 10) {
+      for (let i = 0; i < params.count; i++) {
+        var tweet = data[i];
+        if (tweet !== undefined) {
+          if (tweet.favorite_count > 10 && !tweet.hasOwnProperty("retweeted_status")) {
             console.log(tweet.text);
-            console.log("\nid: " + tweet.id);
-            console.log("\n")
-            lowest_id = tweet.id;
+            console.log("\nid: ", tweet.id);
+            console.log("\n");
           }
+          params.max_id = tweet.id - 1;
         }
       }
-    }
-    params.max_id = lowest_id - 1;
-    T.get('statuses/user_timeline', params, callback);
+      doStuff(i + params.count, params);
+    });
   }
 }
+
+doStuff(0, params);
