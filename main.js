8909da8e03f591f235561c72313d7f36014a4502
Nick Worthington <nworthington16@gmail.com>
2018-06-27 22:07:42 -0400

    Now accepts user input for username and num_likes

--- main.js
+++ main.js
@@ -6,8 +6,7 @@ const credentials = require("./credentials");
 var Twit = require('twit');
 var T = new Twit(credentials);
 
-// console.log("The bot is working \n");
-var params = {screen_name: '16nworthington', count: 200};
+var params = {screen_name: '', count: 200};
 var count = 0;
 var tweets = [];
 
@@ -15,36 +14,40 @@ app.get('/', function(req, res) {
     res.sendfile('index.html');
 });
 
+io.on('connection', function(socket) {
+
+    socket.on('request', function(data) {
+        params.screen_name = data.user;
+        var num_likes = data.num_likes;
+
+        T.get('statuses/user_timeline', params, callback);
+
+        function callback(err, data, response) {
+
+            for (let i = 0; i < params.count; i++) {
+                var tweet = data[i];
+                if (tweet !== undefined) {
+                    if (tweet.favorite_count > num_likes && !tweet.hasOwnProperty("retweeted_status")) {
+                        tweets.push(tweet);
+                        io.emit('sendTweet', {tweet: tweet.text + '\n'});
+                    }
+                    params.max_id = tweet.id - 1;
+                }
+            }
+
+            count++;
+
+            if (count < 16) {
+                T.get('statuses/user_timeline', params, callback);
+            } else {
+                count = 0;
+                params.max_id = tweets[0].id;
+            }
+         }
+    });
+
+});
+
 http.listen(3000, function() {
     console.log('listening on localhost:3000');
 });
-
-io.on('connection', function(socket) {
-    T.get('statuses/user_timeline', params, callback);
-
-    function callback(err, data, response) {
-        for (let i = 0; i < params.count; i++) {
-            var tweet = data[i];
-            if (tweet !== undefined) {
-                if (tweet.favorite_count > 8 && !tweet.hasOwnProperty("retweeted_status")) {
-                    tweets.push(tweet.text);
-                    socket.emit('sendTweet', {tweet: tweet.text + '\n'});
-                }
-                params.max_id = tweet.id - 1;
-            }
-        }
-        count++;
-        if (count < 16) {
-            T.get('statuses/user_timeline', params, callback);
-        }
-     }
-});
-
-
-
-function printTweets() {
-    for (let i = 0; i < tweets.length; i++) {
-        console.log(tweets[i]);
-        console.log("\n");
-    }
-}
