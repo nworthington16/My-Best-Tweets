a3296583f7e7cbff9f67f213dbeca3cd68cb7cfc
Dave Worthington <daveworthington@lawn-143-215-50-190.lawn.gatech.edu>
2018-08-27 01:13:12 -0400

    gitignore

--- credentials.js
+++ credentials.js
@@ -1,6 +1,10 @@
 module.exports = {
-  consumer_key: process.env.consumer_key,
-  consumer_secret: process.env.consumer_secret,
-  access_token: process.env.access_token,
-  access_token_secret: process.env.access_token_secret
+  // consumer_key: process.env.consumer_key,
+  // consumer_secret: process.env.consumer_secret,
+  // access_token: process.env.access_token,
+  // access_token_secret: process.env.access_token_secret
+  consumer_key: "547596254-QXUQxCeKrFa3FXkcAGYBKdK0Ad84Yam4jStpZwX2",
+  consumer_secret: "cv8XATjYaCYGZZUBVLh2MYYYDZJ2VYAipaPe97Jumq1AU",
+  access_token: "HnZhSV2u1l8ekTF9VrLpwNBjj",
+  access_token_secret: "Ls9UlzfA1fXNBHzpyrLgQer23bYRKl4jXvyXpWn0z8jDVCV31P"
 }
